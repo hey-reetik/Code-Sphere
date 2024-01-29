7dea59033b244b7c3c2c92b1aba125ebe38303da
Ritik Sharma <120298818+hey-reetik@users.noreply.github.com>
2024-01-29 22:20:54 +0530

    Screens Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+import Home from './Screens'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    // other pages....
 
+  ])
 
   return (
-    <>
-  <h1>Hi IAM RITIK</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/Screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Styles/Home'
+
+
+function Home() {
+
+
+    return (
+        <>
+            <h1>Hi IAM RITIK</h1>
+        </>
+    )
+}
+
+export default Home
