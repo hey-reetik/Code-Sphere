4450bcfc1dda9fcc403329091f9415cfdb6c8b24
Ritik Sharma <ritiksharma6230@gmail.com>
2024-01-31 12:36:49 +0530

    Home

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '0.2em',
+      widest: '.1em',
+      widest: '0.6em',
+    }
+  },
+  plugins: [],
+}
+
